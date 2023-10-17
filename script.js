2b80674f297bc04ef9f768fdf1265d1a4bcb4fdb
Felipe Borde <fborde.dev@gmail.com>
2023-10-17 00:10:54 -0300

    change avatar photos and links from index

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/avatar-light.png")
-    img.setAttribute("alt", "Foto de Mayk Brito de óculos escuros")
+    img.setAttribute("alt", "Foto de Felipe Borde com seus óculos escuros")
   } else {
     img.setAttribute("src", "./assets/avatar.png")
-    img.setAttribute("alt", "Foto de Mayk Brito")
+    img.setAttribute("alt", "Foto de Felipe Borde com seus fones de ouvido")
   }
 }
